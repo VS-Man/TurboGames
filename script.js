bb65d5b2b53b474047daf8bf3e9e29956d3ac4ab
VS-Man <166947851+VS-Man@users.noreply.github.com>
2024-06-19 12:34:57 +0400

    files added

---
+++ script.js
@@ -0,0 +1,48 @@
+let GIb = document.getElementById("GIb")
+let GIIb = document.getElementById("GIIb")
+let GIIIb = document.getElementById("GIIIb")
+let GIVb = document.getElementById("GIVb")
+let GVb = document.getElementById("GVb")
+let GVIb = document.getElementById("GVIb")
+let fgb = document.getElementById("fgb")
+let hb = document.getElementById("hm")
+let sb = document.getElementById("ad")
+
+function OnGIbclick() {
+    window.location.href = "https://www.adultswim.com/etcetera/choir/"
+}
+function OnGIIbclick() {
+    window.location.href = "https://www.adultswim.com/etcetera/smash-n-dab/"
+}
+function OnGIIIbclick() {
+    window.location.href = "https://www.adultswim.com/etcetera/sidewalk-sidestep/"
+}
+function OnIVclick() {
+    window.location.href = "https://www.adultswim.com/etcetera/elastic-man/"
+}
+function OnVclick() {
+    window.location.href = "https://www.adultswim.com/etcetera/pretty-gross/"
+}
+function OnVIclick() {
+    window.location.href = "http://www.staggeringbeauty.com/"
+}
+function Onfgclick() {
+    window.location.href = "https://www.youtube.com/watch?v=p7YXXieghto"
+}
+function useless() {
+    window.location.href = "https://vs-man.github.io/Game-site/"
+}
+function suggestion() {
+    window.location.href = "https://vs-man.github.io/Redirect/"
+}
+
+sb.addEventListener('click', suggestion)
+GIb.addEventListener('click', OnGIbclick)
+GIIb.addEventListener('click', OnGIIbclick)
+GIIIb.addEventListener('click', OnGIIIbclick)
+GIVb.addEventListener('click', OnIVclick)
+GVb.addEventListener('click', OnVclick)
+GVIb.addEventListener("click", OnVIclick)
+fgb.addEventListener('click', Onfgclick)
+hb.addEventListener('click', useless)
+
